b5f28b71909631f0573debbe89e4d678ab0f8421
Till Brede <tb@symcon.de>
2019-08-28 16:40:26 +0200

    applied cs-fixer style changes

--- Aktivliste/module.php
+++ Aktivliste/module.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 class Aktivliste extends IPSModule
 {
     public function Create()
@@ -8,10 +10,10 @@ class Aktivliste extends IPSModule
         parent::Create();
 
         //Properties
-        $this->RegisterPropertyString("VariableList", "[]");
+        $this->RegisterPropertyString('VariableList', '[]');
 
         //Scripts
-        $this->RegisterScript("TurnOff", $this->Translate("Turn Off"), "<?php\n\nAL_SwitchOff(IPS_GetParent(\$_IPS['SELF']));");
+        $this->RegisterScript('TurnOff', $this->Translate('Turn Off'), "<?php\n\nAL_SwitchOff(IPS_GetParent(\$_IPS['SELF']));");
     }
 
     public function Destroy()
@@ -27,22 +29,22 @@ class Aktivliste extends IPSModule
 
         //Creating array containing variable IDs in List
         $variableIDs = [];
-        $variableList = json_decode($this->ReadPropertyString("VariableList"), true);
+        $variableList = json_decode($this->ReadPropertyString('VariableList'), true);
         foreach ($variableList as $line) {
-            $variableIDs[] = $line["VariableID"];
+            $variableIDs[] = $line['VariableID'];
         }
 
         //Creating links for all variable IDs in VariableList
         foreach ($variableList as $line) {
-            $variableID = $line["VariableID"];
+            $variableID = $line['VariableID'];
             $this->RegisterMessage($variableID, VM_UPDATE);
-            if (!@$this->GetIDForIdent("Link" . $variableID)) {
+            if (!@$this->GetIDForIdent('Link' . $variableID)) {
 
                 //Create links for variables
                 $linkID = IPS_CreateLink();
                 IPS_SetParent($linkID, $this->InstanceID);
                 IPS_SetLinkTargetID($linkID, $variableID);
-                IPS_SetIdent($linkID, "Link" . $variableID);
+                IPS_SetIdent($linkID, 'Link' . $variableID);
 
                 //Setting initial visibility
                 IPS_SetHidden($linkID, (GetValue($variableID) == $this->GetSwitchValue($variableID)));
@@ -52,19 +54,18 @@ class Aktivliste extends IPSModule
         //Deleting unlisted links
         foreach (IPS_GetChildrenIDs($this->InstanceID) as $linkID) {
             if (IPS_LinkExists($linkID)) {
-                if (!in_array(IPS_GetLink($linkID)["TargetID"], $variableIDs)) {
-                    $this->UnregisterMessage(IPS_GetLink($linkID)["TargetID"], VM_UPDATE);
+                if (!in_array(IPS_GetLink($linkID)['TargetID'], $variableIDs)) {
+                    $this->UnregisterMessage(IPS_GetLink($linkID)['TargetID'], VM_UPDATE);
                     IPS_DeleteLink($linkID);
                 }
             }
         }
     }
 
-
     public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     {
         if ($Message == VM_UPDATE) {
-            $linkID = $this->GetIDForIdent("Link" . $SenderID);
+            $linkID = $this->GetIDForIdent('Link' . $SenderID);
             IPS_SetHidden($linkID, $Data[0] == $this->GetSwitchValue($SenderID));
         }
     }
@@ -74,7 +75,7 @@ class Aktivliste extends IPSModule
         foreach (IPS_GetChildrenIDs($this->InstanceID) as $linkID) {
             //Only links
             if (IPS_LinkExists($linkID)) {
-                $targetID = IPS_GetLink($linkID)["TargetID"];
+                $targetID = IPS_GetLink($linkID)['TargetID'];
 
                 if (IPS_VariableExists($targetID)) {
                     $v = IPS_GetVariable($targetID);
@@ -95,45 +96,46 @@ class Aktivliste extends IPSModule
     private function GetSwitchValue($VariableID)
     {
         //Return the value corresponding to the variable type.
-        switch (IPS_GetVariable($VariableID)["VariableType"]) {
+        switch (IPS_GetVariable($VariableID)['VariableType']) {
             //Boolean
             case 0:
                 return $this->IsProfileInverted($VariableID);
 
             //Integer
             case 1:
-            
+
             //Float
             case 2:
                 if (IPS_VariableProfileExists($this->GetVariableProfile($VariableID))) {
                     if ($this->IsProfileInverted($VariableID)) {
-                        return IPS_GetVariableProfile($this->GetVariableProfile($VariableID))["MaxValue"];
+                        return IPS_GetVariableProfile($this->GetVariableProfile($VariableID))['MaxValue'];
                     } else {
-                        return IPS_GetVariableProfile($this->GetVariableProfile($VariableID))["MinValue"];
+                        return IPS_GetVariableProfile($this->GetVariableProfile($VariableID))['MinValue'];
                     }
                 } else {
                     return 0;
                 }
-                                
+
                 // no break
             //Integer
+            // FIXME: No break. Please add proper comment if intentional
             case 3:
-                return "";
-        
-        }      
+                return '';
+
+        }
     }
 
     private function GetVariableProfile($VariableID)
     {
-        $variableProfileName = IPS_GetVariable($VariableID)["VariableCustomProfile"];
-        if ($variableProfileName == "") {
-            $variableProfileName = IPS_GetVariable($VariableID)["VariableProfile"];
+        $variableProfileName = IPS_GetVariable($VariableID)['VariableCustomProfile'];
+        if ($variableProfileName == '') {
+            $variableProfileName = IPS_GetVariable($VariableID)['VariableProfile'];
         }
         return $variableProfileName;
     }
 
     private function IsProfileInverted($VariableID)
     {
-        return substr($this->GetVariableProfile($VariableID), -strlen(".Reversed")) === ".Reversed";
+        return substr($this->GetVariableProfile($VariableID), -strlen('.Reversed')) === '.Reversed';
     }
 }
