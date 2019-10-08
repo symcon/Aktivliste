d52d9acd1d6a90a30966fe66d85e219e09844ede
Till Brede <tb@symcon.de>
2019-10-08 10:37:29 +0200

    fixed style

--- Aktivliste/module.php
+++ Aktivliste/module.php
@@ -14,7 +14,7 @@ class Aktivliste extends IPSModule
 
         //Scripts
         $this->RegisterScript('TurnOff', $this->Translate('Turn Off'), "<?php\n\nAL_SwitchOff(IPS_GetParent(\$_IPS['SELF']));");
-        $this->RegisterReference($this->GetIDForIdent("TurnOff"));
+        $this->RegisterReference($this->GetIDForIdent('TurnOff'));
     }
 
     public function Destroy()
